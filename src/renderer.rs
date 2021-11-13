1a1c58faf6feadf5e5ce5c5583619c84cfd06c7a
Alexander Wauck <waucka@protonmail.com>
2021-11-13 16:42:26 -0600

    Implemented Display for SwapchainImageRef

--- src/renderer.rs
+++ src/renderer.rs
@@ -23,6 +23,12 @@ pub struct SwapchainImageRef {
     pub (crate) idx: u32,
 }
 
+impl std::fmt::Display for SwapchainImageRef {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.idx.fmt(f)
+    }
+}
+
 pub struct Presenter {
     device: Rc<InnerDevice>,
     swapchain: Option<Swapchain>,
